b14949919b191126938e232ada33b9c5fa36bfdf
anildalar <dollorinfotech@gmail.com>
2023-09-26 09:56:29 +0530

    c2

---
+++ src/app/flight/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+          <section className="col-6 a_tbdr">
+            <h1>The Flight</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including ve</p>
+          </section>
+      </>
+  )
+}
---
+++ src/components/layouts/RightAside.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function RightAside() {
+    return (
+        <aside className="col-3 a_tbdr">
+            <h3>What ?</h3>
+            <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like re</p>
+            <h3>Where ?</h3>
+            <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like re</p>
+            <h3>How ?</h3>
+            <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like re</p>
+        </aside>
+    )
+}
